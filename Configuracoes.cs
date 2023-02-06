8e92f1eed1dc74d1039e15fa65066ab2c81d47dc
Lucas Vilas Boas Lage <lucasvilasboaslage@gmail.com>
2023-02-06 13:34:28 -0300

    Commit inicial

---
+++ Configuracoes.cs
@@ -0,0 +1,12 @@
+﻿namespace DeleteBranchs
+{
+    public static class Configuracoes
+    {
+        public const string CAMINHO_PROJETO_FRONT = @"CAMINHO_DO_PROJETO";
+        public const string CAMINHO_PROJETO_BACK = @"CAMINHO_DO_PROJETO";
+
+        public const string GIT_FETCH = "git fetch";
+        public const string GIT_DELETE = "git branch --delete";
+        public const string GIT_BRANCH = "&& git branch -a --merged";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace DeleteBranchs
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool excluirBranch = false;
+            string comando = @$"cd {Configuracoes.CAMINHO_PROJETO_BACK}";
+            Console.WriteLine("Iniciando processo de limpeza...");
+
+            Console.WriteLine("Acessando diretório: " + Configuracoes.CAMINHO_PROJETO_BACK);
+            comando += Configuracoes.GIT_BRANCH;
+            string saida = ExecutarCMD(comando);
+
+            string [] branchs = saida.Split("\n");
+           
+            for (int i = 0; i < branchs.Length; i++)
+            {
+                string branchAtual = branchs[i];
+                if (BranchsParaIgnorar(branchAtual.Trim()))
+                    continue;
+
+                Console.WriteLine("Deletando branch:" + branchAtual);
+                ExecutarCMD($"{Configuracoes.GIT_DELETE} {branchAtual}");
+                excluirBranch = true;
+            }
+
+            if (!excluirBranch)
+                Console.WriteLine("Não existem branchs para excluir");
+
+            Console.WriteLine("Atualizando repositório");
+            ExecutarCMD(Configuracoes.GIT_FETCH);
+        }
+
+        public static bool BranchsParaIgnorar(string branchName)
+        {
+            if (branchName.Contains("develop") || branchName.Contains("remote") || branchName.Contains("*") || branchName == "")
+                return true;
+            else
+                return false;
+        }
+
+        public static string ExecutarCMD(string comando)
+        {
+            using (Process processo = new Process())
+            {
+                processo.StartInfo.FileName = Environment.GetEnvironmentVariable("comspec");
+
+                processo.StartInfo.Arguments = string.Format("/c {0}", comando);
+                processo.StartInfo.RedirectStandardOutput = true;
+                processo.StartInfo.UseShellExecute = false;
+                processo.StartInfo.CreateNoWindow = true;
+
+                processo.Start();
+                processo.WaitForExit();
+                
+                string saida = processo.StandardOutput.ReadToEnd();
+                return saida;
+            }
+        }
+    }
+}
